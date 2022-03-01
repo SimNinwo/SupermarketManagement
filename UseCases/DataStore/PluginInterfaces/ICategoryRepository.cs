ab878a388a2782d8a1b30ea0d323390be6114365
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2022-03-01 14:39:09 +0100

    create category

---
+++ UseCases/DataStore/PluginInterfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases.DataStore.PluginInterfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetCategories();
+    }
+}
--- UseCases/ViewCategoriesUseCase.cs
+++ UseCases/ViewCategoriesUseCase.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using UseCases.DataStore.PluginInterfaces;
 
 namespace UseCases
 {
     public class ViewCategoriesUseCase
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public IEnumerable<Category> Execute()
+        {
+            return _categoryRepository.GetCategories();
+        }
     }
 }
